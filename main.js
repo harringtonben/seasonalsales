8ff7fe1fede9aeea468d6611a818dd28dea959c9
ben harrington <harringtonben@icloud.com>
2017-08-24 20:33:36 -0500

    added event listener

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 console.log('in main js');
 
 var productArray = [];
-
-
-
-
+var categoryArray = [];
+var dropDown = document.getElementById('dropdown');
+var slectedDiscount;
 
 function productsJSONConvert() {
 	// console.log("this", this.responseText);
 	var data = JSON.parse(this.responseText);
 	productArray = data.products;
 	// console.log(productArray);
-	printProducts(productArray);
+	printProducts(productArray);	
+}
+
+function categoriesJSONConvert() {
+	// console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	categoryArray = data.categories;
+	// console.log(productArray);
+	// console.log(categoryArray);
 }
 
 function executeThisCodeIfFileErrors() {
@@ -35,6 +42,34 @@ function printToPage(strang){
 	document.getElementById('products').innerHTML += strang;
 }
 
+
+dropDown.addEventListener('change', function(event) {
+	if (event.target.options[1].selected === true) {
+		selectedDiscount = event.target.value;
+		addDiscount(1);
+	} else if (event.target.options[2].selected === true) {
+		addDiscount(2);
+		selectedDiscount = event.target.value;
+	} else if (event.target.options[3].selected === true) {
+		addDiscount(3);
+		selectedDiscount = event.target.value;
+	}
+	// addDiscount();
+});
+
+
+
+
+function addDiscount(season) {
+	for (var i = 0; i < productArray.length; i++) {
+		if (season === 1 && productArray[i].category_id === 1) {
+			
+		} else if (season === 2 && productArray[i].category_id === 2) {
+			console.log('discount 2!!');
+		}
+	}
+}
+
 //call products
 var productsRequest = new XMLHttpRequest();
 productsRequest.addEventListener("load", productsJSONConvert);
@@ -42,4 +77,11 @@ productsRequest.addEventListener("error", executeThisCodeIfFileErrors);
 productsRequest.open("GET", "products.json");
 productsRequest.send();
 
+
+//call categories
+var categoryRequest = new XMLHttpRequest();
+categoryRequest.addEventListener("load", categoriesJSONConvert);
+categoryRequest.addEventListener("error", executeThisCodeIfFileErrors);
+categoryRequest.open("GET", "categories.json");
+categoryRequest.send();
 // console.log(myRequest);
