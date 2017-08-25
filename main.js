b3e97d091fc67700b271213e9031564d5f7b2499
ben harrington <harringtonben@icloud.com>
2017-08-25 18:57:31 -0500

    almost done just need one more thing

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var productArray = [];
 var categoryArray = [];
 var dropDown = document.getElementById('dropdown');
 var slectedDiscount;
+var newProductArray = [];
 
 function productsJSONConvert() {
 	var data = JSON.parse(this.responseText);
@@ -38,35 +39,33 @@ function printToPage(strang){
 
 
 dropDown.addEventListener('change', function(event) {
+	newProductArray = productArray;
 	if (event.target.options[1].selected === true) {
-		console.log(event);
 		selectedDiscount = event.target.value;
-		addDiscount(1, productArray);
-		console.log(selectedDiscount);
+		addDiscount(1, newProductArray);
 	} else if (event.target.options[2].selected === true) {
 		selectedDiscount = event.target.value;
-		addDiscount(2, productArray);
-		console.log(selectedDiscount);
+		addDiscount(2, newProductArray);
 	} else if (event.target.options[3].selected === true) {
-		addDiscount(3, productArray);
 		selectedDiscount = event.target.value;
-		console.log(selectedDiscount);
+		addDiscount(3, newProductArray);
 	}
 
 });
 
 function addDiscount(category, product ) {
+	// console.log(productArray);
 	for (var i = 0; i < product.length; i++) {
 		if (product[i].category_id === category) {
 			// debugger;
 			// console.log(product[i]);
 			var newPrice = product[i].price-(product[i].price*categoryArray[category-1].discount);
 			product[i].price = newPrice;
-			productArray = product;
 		} 
 	}
-
-	printProducts(product);
+	newProductArray = product;
+	// console.log(newProductArray);
+	printProducts(newProductArray);
 }
 
 //call products
