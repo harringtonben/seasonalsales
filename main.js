f40a33f453e60e5373a554bf670b125646482f73
ben harrington <harringtonben@icloud.com>
2017-08-26 12:29:25 -0500

    so close but not quite there

--- main.js
+++ main.js
@@ -6,21 +6,26 @@ var dropDown = document.getElementById('dropdown');
 var slectedDiscount;
 var newProductArray = [];
 
-function productsJSONConvert() {
-	var data = JSON.parse(this.responseText);
-	productArray = data.products;
-	newProductArray = productArray;
-	// console.log(newProductArray)
-	printProducts(productArray);	
-}
-
 function categoriesJSONConvert() {
 	var data = JSON.parse(this.responseText);
 	categoryArray = data.categories;
 }
 
+function productsJSONConvert() {
+	var data = JSON.parse(this.responseText);
+	productArray = data.products;
+	createNewProduct(productArray);
+	printProducts(newProductArray);	
+}
+
 function executeThisCodeIfFileErrors() {
 	console.log("shit broke");
+}
+
+function createNewProduct(products) {
+products.forEach(function(object) {
+	newProductArray.push(object);
+});
 }
 
 function printProducts(products) {
@@ -28,6 +33,7 @@ function printProducts(products) {
 	for (var i = 0; i < products.length; i++) {
 		
 		domString += `<div class="productCard">
+					   <img class="pics" src="${products[i].image}">
 					   <h1>${products[i].name}</h1>
 					   <h3>${products[i].price}</h3>
 					  </div>`;
@@ -39,34 +45,26 @@ function printToPage(strang){
 	document.getElementById('products').innerHTML = strang;
 }
 
-
 dropDown.addEventListener('change', function(event) {
-	// printProducts(productArray);
 	if (event.target.options[1].selected === true) {
-		selectedDiscount = event.target.value;
 		addDiscount(1, newProductArray);
 	} else if (event.target.options[2].selected === true) {
-		selectedDiscount = event.target.value;
 		addDiscount(2, newProductArray);
 	} else if (event.target.options[3].selected === true) {
-		selectedDiscount = event.target.value;
 		addDiscount(3, newProductArray);
 	}
-
 });
 
 function addDiscount(category, product ) {
-	// console.log(productArray);
 	for (var i = 0; i < product.length; i++) {
 		if (product[i].category_id === category) {
-			// debugger;
-			// console.log(product[i]);
 			var newPrice = product[i].price-(product[i].price*categoryArray[category-1].discount);
+			newPrice = newPrice.toFixed(2);
 			product[i].price = newPrice;
 		} 
 	}
 	newProductArray = product;
-	// console.log(newProductArray);
+	console.log(productArray);
 	printProducts(newProductArray);
 }
 
